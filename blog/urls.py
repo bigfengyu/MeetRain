a5d47f1856567b1144c0672c8a6e149b03ec86f0
lolyu <369892790qq@sina.com>
2017-03-02 02:31:23 +0800

    fix urls bug

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = patterns('',
                        url(r'^$',views.index,name='index'),
+                       url(r'^page/(?P<slug>\w+)/$',views.pageslug,name='pageslug'),
                        url(r'^page/$',views.pageid,name='pageid'),
                        url(r'^about/$',views.indexAbout,name='indexabout'),
-                       url(r'^page/(?P<slug>\w+)/$',views.pageslug,name='pageslug'),
                        url(r'^get_more/',views.get_more,name='get_more'),
                        url(r'^tags/$',views.indexTags,name='indextags'),
                        url(r'^tags/get_more',views.getMorePagesInTags,name='getMorePagesInTags'),
